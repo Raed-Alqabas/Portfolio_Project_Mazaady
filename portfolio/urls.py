1de4b043a6e176160a0b3db8d53d40df8b671522
Rayan Hantoul <ryan-055@outlook.com>
2025-12-08 20:49:10 +0300

    login form

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
 from . import views
-
-app_name = 'portfolio'
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.home, name='home'),
+         path('', views.index, name ='index'),
 ]
