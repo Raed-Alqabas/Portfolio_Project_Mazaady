6422cd385b6cbf988e88f9ffe2a0f4a421a747a0
vorw <sultanalsanie23@gmail.com>
2026-01-06 20:09:35 +0300

    trying to fix payment issues and connecting React to the backend

--- portfolio/serializers.py
+++ portfolio/serializers.py
@@ -1,35 +1,35 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ["id", "username", "email", "password"]
-        extra_kwargs = {"password": {"write_only": True}}
-
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        return user
+from .models import Profile
 
 class RegisterSerializer(serializers.ModelSerializer):
+    phone_number = serializers.CharField(write_only=True)
+    phone_country_code = serializers.CharField(write_only=True, required=False, default="966")
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
+
     class Meta:
         model = User
-        fields = ('username', 'email', 'password')
+        fields = ("username", "email", "password", "first_name", "last_name", "phone_country_code", "phone_number")
         extra_kwargs = {
-            'password': {'write_only': True},
-            'email': {'required': True}
+            "password": {"write_only": True},
+            "email": {"required": True},
         }
 
     def validate_email(self, value):
-        """Ensure email is unique"""
         if User.objects.filter(email=value).exists():
             raise serializers.ValidationError("هذا البريد الإلكتروني مستخدم بالفعل")
         return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password']
+        phone_number = validated_data.pop("phone_number")
+        phone_country_code = validated_data.pop("phone_country_code", "966")
+
+        user = User.objects.create_user(**validated_data)
+
+        Profile.objects.create(
+            user=user,
+            phone_country_code=phone_country_code,
+            phone_number=phone_number,
         )
         return user
