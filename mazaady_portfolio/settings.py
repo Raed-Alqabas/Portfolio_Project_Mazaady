2dd76db7510b1779eae0d4fcb6fe7c47770bd211
vorw <sultanalsanie23@gmail.com>
2026-01-17 20:38:27 +0300

    changed to mysql

--- mazaady_portfolio/settings.py
+++ mazaady_portfolio/settings.py
@@ -131,8 +131,12 @@ WSGI_APPLICATION = 'mazaady_portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mazaady_db',
+        'USER': 'admin',
+        'PASSWORD': 'admin@AdMiN123',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
